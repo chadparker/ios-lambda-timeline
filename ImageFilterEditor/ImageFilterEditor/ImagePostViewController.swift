d5825875d93c6c7a2f9a80daed995d1108c9a060
Chad Parker <chad@chadparker.com>
2020-07-08 20:21:56 -0700

    Added UIState, blur filter button

--- ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
+++ ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
@@ -12,17 +12,60 @@ import Photos
 
 class ImagePostViewController: UIViewController {
 
+    // MARK: - Outlets
+
     @IBOutlet weak var selectImageButtonContainer: UIView!
     @IBOutlet weak var selectImageButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var blurButton: UIButton!
+
+
+    // MARK: - Properties
 
     var originalImage: UIImage?
+    var currentFilter: CIFilter?
+
+
+    // MARK: - UI State
+
+    enum UIState {
+        case noPhoto
+        case photoPicked(UIImage)
+        case selectFilter(CIFilter)
+    }
+
+    var state: UIState = .noPhoto {
+        didSet {
+            updateUI()
+        }
+    }
+
+
+    // MARK: - UI Updates
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         selectImageButton.layer.cornerRadius = 16
+        blurButton.layer.cornerRadius = 8
+        blurButton.layer.borderWidth = 0.5
     }
+
+    private func updateUI() {
+        switch state {
+        case .noPhoto:
+            selectImageButtonContainer.isHidden = false
+        case .photoPicked(let image):
+            originalImage = image
+            imageView.image = image
+            selectImageButtonContainer.isHidden = true
+        case .selectFilter(let filter):
+            print(filter)
+        }
+    }
+
+
+    // MARK: - Actions
 
     @IBAction func selectImage(_ sender: Any) {
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
@@ -36,16 +79,23 @@ class ImagePostViewController: UIViewController {
 
         present(imagePicker, animated: true, completion: nil)
     }
+
+    @IBAction func blur(_ sender: Any) {
+
+    }
 }
+
+
+// MARK: - Image Picker Delegate
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 
         if let image = info[.editedImage] as? UIImage {
-            originalImage = image
+            state = .photoPicked(image)
         } else if let image = info[.originalImage] as? UIImage {
-            originalImage = image
+            state = .photoPicked(image)
         }
 
         picker.dismiss(animated: true, completion: nil)

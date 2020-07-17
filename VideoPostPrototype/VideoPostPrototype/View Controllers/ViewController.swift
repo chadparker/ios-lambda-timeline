fd0372e0474128574c6612a6b2f3b5358ab38870
Chad Parker <chad@chadparker.com>
2020-07-16 19:47:33 -0700

    Update ViewController to request Camera permnission

--- VideoPostPrototype/VideoPostPrototype/View Controllers/ViewController.swift
+++ VideoPostPrototype/VideoPostPrototype/View Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        requestVideoPermission()
+    }
+
+    private func requestPermissionAndShowCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            requestVideoPermission()
+        case .restricted:
+            preconditionFailure("Video is disabled. Please review device restrictions.")
+        case .denied:
+            preconditionFailure("Tell user to change settings > privacy > video.")
+        case .authorized:
+            showCamera()
+        @unknown default:
+            preconditionFailure("New status code to handle")
+        }
+    }
+
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { isGranted in
+            guard isGranted else {
+                preconditionFailure("UI: Tell the user to enable permissions for video/camera")
+            }
+
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
 
 }
-
